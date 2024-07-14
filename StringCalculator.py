68e4dcaa4a8a2ad663e6cb6d95ac9e70ed00a84f
shc32 <shc146388@gmail.com>
2024-07-14 13:52:47 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,4 +1,20 @@
-def add(input):
-  num_of_arg = len(input.split(","))
-  if input:
-    return 0
+import re
+
+def add(numbers_string):
+    if not numbers_string:
+        return 0
+    
+    delimiter = ","
+    if numbers_string.startswith("//"):
+        custom_delimiter_match = re.match(r"//\[?(.+?)\]?\n", numbers_string)
+        if custom_delimiter_match:
+            delimiter = custom_delimiter_match.group(1)
+            numbers_string = numbers_string[len(custom_delimiter_match.group(0)):]
+    
+    numbers = re.split(f"{delimiter}|\n", numbers_string)
+    negatives = [int(num) for num in numbers if int(num) < 0]
+    if negatives:
+        raise Exception(f"negatives not allowed: {', '.join(map(str, negatives))}")
+    
+    return sum(int(num) for num in numbers if 0 <= int(num) <= 1000)
+
