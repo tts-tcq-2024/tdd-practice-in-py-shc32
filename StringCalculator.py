6c18519edfdc815dbacfd260019ba8f62257adcd
shc32 <shc146388@gmail.com>
2024-07-16 15:36:09 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,29 +1,27 @@
-import re
+class StringCalculator:
 
-def parse_numbers(numbers_string):
-    delimiter = ","
-    if numbers_string.startswith("//"):
-        custom_delimiter_match = re.match(r"//\[?(.+?)\]?\n", numbers_string)
-        if custom_delimiter_match:
-            delimiter = custom_delimiter_match.group(1)
-            numbers_string = numbers_string[len(custom_delimiter_match.group(0)):]
+    @staticmethod
+    def add(numbers_string):
+        if not numbers_string:
+            return 0
+        
+        delimiter = ","
+        if numbers_string.startswith("//"):
+            custom_delimiter_match = re.match(r"//(\[?.+?\]?)\n", numbers_string)
+            if custom_delimiter_match:
+                delimiter = custom_delimiter_match.group(1)
+                numbers_string = numbers_string[len(custom_delimiter_match.group(0)):]
 
-    numbers = re.split(f"{delimiter}|\n", numbers_string)
-    return numbers
+        numbers = re.split(f"{delimiter}|\n", numbers_string)
+        
+        negatives = [int(num) for num in numbers if int(num) < 0]
+        if negatives:
+            raise Exception(f"negatives not allowed: {', '.join(map(str, negatives))}")
+        
+        filtered_numbers = [int(num) for num in numbers if 0 <= int(num) <= 1000]
+        
+        return sum(filtered_numbers)
 
-def add(numbers_string):
-    if not numbers_string:
-        return 0
-    
-    numbers = parse_numbers(numbers_string)
-    
-    negatives = [int(num) for num in numbers if int(num) < 0]
-    if negatives:
-        raise Exception(f"negatives not allowed: {', '.join(map(str, negatives))}")
-    
-    filtered_numbers = [int(num) for num in numbers if 0 <= int(num) <= 1000]
-    
-    return sum(filtered_numbers)
 
 
 
