f37ea6e9852814d37007d7225038155d5a4ec2b4
shc32 <shc146388@gmail.com>
2024-07-20 00:25:50 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,10 +1,10 @@
 import re
 
-def sum_of_numbers(input):
-    return sum(map(int, re.findall('\d+', input))) 
+#def sum_of_numbers(input):
+#    return sum(map(int, re.findall('\d+', input))) 
     
 def add(input):
     if (input == "0") or (input == ""):
-        return 0
-    else:
-        sum_of_numbers(input)
+        print(0)
+#    else:
+#        sum_of_numbers(input)
