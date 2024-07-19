19fe194f5d539cd4b9c7026b33c43f3fa8e03a4d
shc32 <shc146388@gmail.com>
2024-07-19 18:25:45 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,19 +1,10 @@
 import re
 
 def sum_of_numbers(str):
-    return sum(map(int, re.findall('\d+', str)))
+    return sum(map(int, re.findall('\d+', str))) 
     
 def add(input):
     if (input == "0") or (input == ""):
         return 0
-    else:
-        sum_of_numbers(input)
-
-
-
-
-
-
-
-
-
+        
+    sum_of_numbers(input)
