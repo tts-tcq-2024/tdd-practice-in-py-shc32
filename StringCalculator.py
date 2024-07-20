379fb4442661ce9ab955bedb4d2275e268adc2a6
shc32 <shc146388@gmail.com>
2024-07-20 08:10:28 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -3,7 +3,7 @@
 #def sum_of_numbers(input):
 #    return sum(map(int, re.findall('\d+', input)))
     
-def add(self, input):
+def add(input):
     if (input == "0") or (input == ""):
         return 0
 
