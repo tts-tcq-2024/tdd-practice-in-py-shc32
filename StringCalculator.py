ba6a88b16367ed241fc38f2b9ccb13e302ab767b
shc32 <shc146388@gmail.com>
2024-07-20 08:29:28 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -8,21 +8,24 @@ def add(input):
         delimiter = input[2:delimiter_index]
         input = input[delimiter_index+1:]
     
-    # Split by delimiter, commas, or new lines
+    numbers = parse_numbers(input, delimiter)
+    handle_negatives(numbers)
+    numbers = ignore_large_numbers(numbers)
+    
+    return sum(numbers)
+
+def parse_numbers(input, delimiter):
     numbers = []
     for part in input.split('\n'):
         numbers.extend(part.split(delimiter))
-    
-    # Filter out empty strings and convert to integers
-    numbers = [int(num) for num in numbers if num != ""]
-    
-    # Check for negatives
+    return [int(num) for num in numbers if num != ""]
+
+def handle_negatives(numbers):
     negatives = [num for num in numbers if num < 0]
     if negatives:
         raise ValueError("negatives not allowed: " + ",".join(map(str, negatives)))
-    
-    # Ignore numbers larger than 1000
-    numbers = [num for num in numbers if num <= 1000]
-    
-    return sum(numbers)
+
+def ignore_large_numbers(numbers):
+    return [num for num in numbers if num <= 1000]
+
 
