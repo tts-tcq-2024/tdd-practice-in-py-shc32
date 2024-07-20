d11ebcdd8ae7dc0beb58983d273cb7e1d0e5217b
shc32 <shc146388@gmail.com>
2024-07-20 08:01:47 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,10 +1,22 @@
-import re
+#import re
 
-def sum_of_numbers(input):
-    return sum(map(int, re.findall('\d+', input))) 
+#def sum_of_numbers(input):
+#    return sum(map(int, re.findall('\d+', input)))
+
+def findSum(input):
+    temp = "0"
+    Sum = 0
+    for ch in input:
+        if (ch.isdigit()):
+            temp += ch
+        else:
+            Sum += int(temp)
+            temp = "0"
+            
+    return Sum + int(temp)
     
 def add(input):
-    sum_of_numbers(input)
+    findSum(input)
 #    if (input == "0") or (input == ""):
 #        return 0
 #    else:
