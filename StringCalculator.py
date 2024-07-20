3599325cf0d6132ed7d227c97d09c743f813b446
shc32 <shc146388@gmail.com>
2024-07-20 08:13:01 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,14 +1,15 @@
 #import re
 
-#def sum_of_numbers(input):
+def sum_of_numbers(input):
+    numbers = input.split(',')
+    return sum(int(num) for num in numbers)
 #    return sum(map(int, re.findall('\d+', input)))
     
 def add(input):
     if (input == "0") or (input == ""):
         return 0
 
-    numbers = input.split(',')
-    return sum(int(num) for num in numbers)
-    
+    sum_of_numbers(input)
+  
 #    else:
 #        sum_of_numbers(input)
